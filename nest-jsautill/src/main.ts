226dd0d1df978f9fa840eb4bfe14706b590ed31d
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2025-07-18 11:02:55 +0200

    cors

--- nest-jsautill/src/main.ts
+++ nest-jsautill/src/main.ts
@@ -4,20 +4,18 @@ import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
 
   /*app.setGlobalPrefix('api/v1');
   app.use(function (request: Request, response: Response, next: NextFunction) {
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     next();
   });*/
-
-  /*
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: 'http://localhost:4200',
     methods: '*',
     credentials: true,
-  });*/
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
