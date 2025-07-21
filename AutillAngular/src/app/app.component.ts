65ec53172c356b29cad230e50e49f6a94e233cd8
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2025-07-21 11:14:31 +0200

    modern

--- AutillAngular/src/app/app.component.ts
+++ AutillAngular/src/app/app.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./core/components/footer/footer.component";
 import { HeaderComponent } from './core/components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   isLogin: boolean = false;
   dataComplete = false;
+  isMenuOpen: boolean = false;
 
   constructor(private router: Router) {
   }
@@ -20,12 +22,13 @@ export class AppComponent {
   ngOnInit() {
     this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationEnd){
-        if(event.url === '/'){
-          this.isLogin = true;
-        }else{
-          this.isLogin = false;
-        }
+        if(event.url === '/'){ this.isLogin = true; }
+        else { this.isLogin = false; }
       }
     })
+  }
+
+  onMenuStateChange(open: boolean) {
+    this.isMenuOpen = open;
   }
 }
