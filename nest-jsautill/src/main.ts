9dd26b54a54b52911563530504ab7888d1a364b2
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2025-07-25 11:59:09 +0200

    asi

--- nest-jsautill/src/main.ts
+++ nest-jsautill/src/main.ts
@@ -1,35 +1,21 @@
-// src/main.ts
-console.log('--- VERCEL DEBUG: STARTING NESTJS SERVERLESS BOOTSTRAP ---');
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Callback, Context, Handler } from 'aws-lambda';
-import serverlessExpress from '@vendia/serverless-express';
+import { Request, Response, NextFunction } from 'express';
 
-let cachedServer: Handler;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
-async function bootstrapServer(): Promise<Handler> {
-  console.log('--- VERCEL DEBUG: INSIDE bootstrapServer FUNCTION---');
-  try {
-    const app = await NestFactory.create(AppModule);
-
-    app.enableCors({
-      origin: 'https://autill-front.vercel.app',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    });
-
-    await app.init();
-    console.log('--- VERCEL DEBUG: NestJS App Initialized ---');
-    const expressApp = app.getHttpAdapter().getInstance();
-    return serverlessExpress({ app: expressApp });
-  } catch (error) {
-    console.error('--- VERCEL DEBUG: ERROR DURING NESTJS BOOTSTRAP ---', error);
-    throw error;
-  }
+  /*app.setGlobalPrefix('api/v1');
+  app.use(function (request: Request, response: Response, next: NextFunction) {
+    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    next();
+  });*/
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: 'https://autill-front.vercel.app',
+    methods: '*',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT || 3000);
 }
-
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  console.log('--- VERCEL DEBUG: HANDLER INVOKED --- Request Path:', event.path);
-  cachedServer = cachedServer ?? (await bootstrapServer());
-  return cachedServer(event, context, callback);
-};
+bootstrap();
