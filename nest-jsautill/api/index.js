aa8df0e6709e0e06e8210ae986b34fad6e73be5c
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2025-07-25 11:27:51 +0200

    main

--- nest-jsautill/api/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res) => {
-  console.log('Simple API function called!');
-  res.status(200).json({ message: 'Hello from Vercel Simple API!', path: req.url });
-};
--- nest-jsautill/src/main.ts
+++ nest-jsautill/src/main.ts
@@ -1,30 +1,35 @@
-console.log('--- VERCEL: INICIO DE NESTJS HANDLER ---');
+// src/main.ts
+console.log('--- VERCEL DEBUG: STARTING NESTJS SERVERLESS BOOTSTRAP ---');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Callback, Context, Handler } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
 
-let cachedServer: Handler; 
+let cachedServer: Handler;
 
 async function bootstrapServer(): Promise<Handler> {
-  const app = await NestFactory.create(AppModule);
+  console.log('--- VERCEL DEBUG: INSIDE bootstrapServer FUNCTION ---');
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://autill-front.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+    app.enableCors({
+      origin: 'https://autill-front.vercel.app',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
 
-  await app.init(); 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+    await app.init();
+    console.log('--- VERCEL DEBUG: NestJS App Initialized ---');
+    const expressApp = app.getHttpAdapter().getInstance();
+    return serverlessExpress({ app: expressApp });
+  } catch (error) {
+    console.error('--- VERCEL DEBUG: ERROR DURING NESTJS BOOTSTRAP ---', error);
+    throw error;
+  }
 }
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  console.log('--- VERCEL DEBUG: HANDLER INVOKED --- Request Path:', event.path);
   cachedServer = cachedServer ?? (await bootstrapServer());
   return cachedServer(event, context, callback);
 };
