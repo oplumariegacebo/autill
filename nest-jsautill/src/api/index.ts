a154cbdee8eb596d75eca340593c25fd9e25f68d
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2025-07-25 09:47:15 +0200

    new conf

--- nest-jsautill/src/api/index.ts
+++ nest-jsautill/src/api/index.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
+import { AppModule } from '../app.module';
 import * as express from 'express';
 
 const server = express();
