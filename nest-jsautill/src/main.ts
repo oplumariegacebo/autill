c280070a88e99bd24be7ca0f01c8ee0648b82fb1
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2025-07-25 10:30:23 +0200

    op

--- nest-jsautill/src/main.ts
+++ nest-jsautill/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'https://autill-front.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
